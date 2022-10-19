0b2475dd27c7716f4628574dc21e91dbe9f28833
Hakon Ingvaldsen <hakon.ingvaldsen@gmail.com>
2022-10-19 17:00:48 +0200

    Setter Cosmos versjonen som default

--- final/Clippers.EventFlow/src/Clippers.EventFlow/Program.cs
+++ final/Clippers.EventFlow/src/Clippers.EventFlow/Program.cs
@@ -19,28 +19,28 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddLogging();
 //*************** This is injected for CDE Version (CosmosDB)**************
-//builder.Services.AddSingleton(new CosmosClient("https://localhost:8081",
-//                    "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="));
-//builder.Services.AddSingleton<IEventStore>(
-//    new CdeEventStore(
-//        "https://localhost:8081",
-//        "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
-//        "eventsdb")
-//);
+builder.Services.AddSingleton(new CosmosClient("https://localhost:8081",
+                    "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="));
+builder.Services.AddSingleton<IEventStore>(
+    new CdeEventStore(
+        "https://localhost:8081",
+        "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+        "eventsdb")
+);
 //************** END CDE Injection *****************************************
 
 //*************** This is injected for Outbox Version (MongoDB)  **************
-builder.Services.AddSingleton<IMongoClient>(new MongoClient("mongodb://localhost:27017")); //Options, If you are using MongoDB
-builder.Services.AddSingleton<ISubscriber, Subscriber>();
-builder.Services.AddCap(x =>
-{
-    //x.UseInMemoryStorage();
-    x.UseMongoDB("localhost:27017");
-    x.UseInMemoryMessageQueue();
-});
+//builder.Services.AddSingleton<IMongoClient>(new MongoClient("mongodb://localhost:27017")); //Options, If you are using MongoDB
+//builder.Services.AddSingleton<ISubscriber, Subscriber>();
+//builder.Services.AddCap(x =>
+//{
+//    //x.UseInMemoryStorage();
+//    x.UseMongoDB("localhost:27017");
+//    x.UseInMemoryMessageQueue();
+//});
 
-builder.Services.AddSingleton<IEventStore, OutboxEventStore>();
-builder.Services.AddSingleton<IViewRepository, OutboxViewRepository>();
+//builder.Services.AddSingleton<IEventStore, OutboxEventStore>();
+//builder.Services.AddSingleton<IViewRepository, OutboxViewRepository>();
 //************** END Outbox Injection *****************************************
 
 builder.Services.AddScoped<IPurchaseHaircutService, PurchaseHaircutService>();
