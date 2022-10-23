e7fe5861f95bffb6c292de48ff1bd01bd65d4b9a
Hakon Ingvaldsen <hakon.ingvaldsen@gmail.com>
2022-10-23 22:05:33 +0200

    Some code cleanup

--- final/Clippers.EventFlow.Projections/src/Clippers.EventFlow.Projections.Infrastructure/SignalR/NotificationHub.cs
+++ final/Clippers.EventFlow.Projections/src/Clippers.EventFlow.Projections.Infrastructure/SignalR/NotificationHub.cs
@@ -5,35 +5,15 @@ namespace Clippers.EventFlow.Projections.Infrastructure.SignalR
 {
     public interface INotificationHubClient
     {
-        Task SendNotification2(string message);
+        Task SendNotification(string message);
     }
     public class NotificationHub : Hub, INotificationHubClient
     {
-        //private IHubContext<NotificationHub, INotificationHubClient> _notificationHub;
-        //public NotificationHub(IHubContext<NotificationHub, INotificationHubClient> notificationHub)
-        //{
-        //    _notificationHub = notificationHub;
-        //}
-
-        //private IHubContext<NotificationHub> _notificationHub;
-        //public NotificationHub(IHubContext<NotificationHub> notificationHub) 
-        //{
-        //    _notificationHub = notificationHub;
-        //}
-
-        public async override Task OnConnectedAsync()
-        {
-            await base.OnConnectedAsync();
-            await SendNotification("Testing");
-        }
         public async Task SendNotification(string message)
         {
             await Clients.All.SendAsync("SendNotification",message);
         }
 
-        public Task SendNotification2(string message)
-        {
-            throw new NotImplementedException();
-        }
+      
     }
 }
