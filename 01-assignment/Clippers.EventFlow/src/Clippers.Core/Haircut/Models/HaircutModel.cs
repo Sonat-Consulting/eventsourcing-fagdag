50b917047fb64711aa1aa8536be507b535da0555
Hakon Ingvaldsen <hakon.ingvaldsen@gmail.com>
2022-10-24 18:38:29 +0200

    Stripped code for assignment 1

---
+++ 01-assignment/Clippers.EventFlow/src/Clippers.Core/Haircut/Models/HaircutModel.cs
@@ -0,0 +1,81 @@
+﻿using Clippers.Core.EventStore;
+using Clippers.Core.Haircut.Events;
+using System.Text.Json.Serialization;
+
+namespace Clippers.Core.Haircut.Models
+{
+    public class HaircutModel
+    {
+        public string HaircutId { get; private set; }
+        public string CustomerId { get; private set; }
+        public string HairdresserId { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? StartedAt { get; private set; } = null;
+        public string DisplayName { get; private set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public HaircutStatusType HaircutStatus { get; private set; }
+        public int Version { get; }
+        public List<IEvent> Changes { get; } = new List<IEvent>();
+
+        public HaircutModel(string haircutId, string customerId, string displayName, DateTime createdAt)
+        {
+            Apply(new HaircutCreated
+            {
+                HaircutId = haircutId,
+                CustomerId = customerId,
+                DisplayName = displayName,
+                CreatedAt = createdAt
+            });
+        }
+
+        public HaircutModel(IEnumerable<IEvent> events)
+        {
+            foreach (var @event in events)
+            {
+                Mutate(@event);
+                Version += 1;
+            }
+        }
+        private void Mutate(IEvent @event)
+        {
+            ((dynamic)this).When((dynamic)@event);
+        }
+
+        private void Apply(IEvent @event)
+        {
+            Changes.Add(@event);
+            Mutate(@event);
+        }
+
+        public void Start(string hairdresserId, DateTime startedAt)
+        {
+            if (HaircutStatus != HaircutStatusType.waiting)
+            {
+                throw new ArgumentException("You can only start waiting customers.");
+            }
+            Apply(new HaircutStarted
+            {
+                HaircutId = HaircutId,
+                HairdresserId = hairdresserId,
+                StartedAt = startedAt,
+            });
+        }
+
+        private void When(HaircutCreated @event)
+        {
+            HaircutId = @event.HaircutId;
+            CustomerId = @event.CustomerId;
+            DisplayName = @event.DisplayName;
+            CreatedAt = @event.CreatedAt;
+            HaircutStatus = HaircutStatusType.waiting;
+        }
+
+        private void When(HaircutStarted @event)
+        {
+            StartedAt = @event.StartedAt;
+            HairdresserId = @event.HairdresserId;
+            HaircutStatus = HaircutStatusType.serving;
+        }
+    }
+}
---
+++ 01-assignment/Clippers.EventFlow/src/Clippers.EventFlow/Program.cs
@@ -0,0 +1,93 @@
+using Clippers.Core.EventStore;
+using Clippers.Core.Haircut.Events;
+using Clippers.Core.Haircut.Repository;
+using Clippers.Core.Haircut.Services;
+using Clippers.Infrastructure.EventStore;
+using Clippers.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Swashbuckle.AspNetCore.Annotations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddLogging();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost");
+        builder.AllowAnyHeader();
+        builder.AllowAnyMethod();
+    });
+});
+
+//*************** This is injected for CDE Version (CosmosDB)**************
+builder.Services.AddSingleton(new CosmosClient("https://localhost:8081",
+                    "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="));
+builder.Services.AddSingleton<IEventStore>(
+    new CdeEventStore(
+        "https://localhost:8081",
+        "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+        "eventsdb")
+);
+//************** END CDE Injection *****************************************
+
+//*************** This is injected for Outbox Version (MongoDB)  **************
+//builder.Services.AddSingleton<IMongoClient>(new MongoClient("mongodb://localhost:27017")); //Options, If you are using MongoDB
+//builder.Services.AddSingleton<ISubscriber, Subscriber>();
+//builder.Services.AddCap(x =>
+//{
+//    //x.UseInMemoryStorage();
+//    x.UseMongoDB("localhost:27017");
+//    x.UseInMemoryMessageQueue();
+//});
+
+//builder.Services.AddSingleton<IEventStore, OutboxEventStore>();
+//builder.Services.AddSingleton<IViewRepository, OutboxViewRepository>();
+//************** END Outbox Injection *****************************************
+
+builder.Services.AddScoped<ICreateHaircutService, CreateHaircutService>();
+builder.Services.AddScoped<IStartHaircutService, StartHaircutService>();
+builder.Services.AddScoped<IHaircutRepository, HaircutRepository>();
+
+builder.Services.AddSwaggerGen(opts => opts.EnableAnnotations());
+
+var app = builder.Build();
+
+app.UseCors();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/createHaircut", async ([FromBody] CreateHaircutCommand createHaircutCommand, [FromServices] ICreateHaircutService createHaircutService) =>
+{
+    var ret = await createHaircutService.CreateHaircut(createHaircutCommand);
+    return ret;
+}).WithMetadata(new SwaggerOperationAttribute(summary: "Create a haircut in the EventStore.", description: "Creates the haircut. Returns the Haircut with its new HaircutId, in status waiting."));
+
+app.MapPost("/startHaircut", async ([FromBody] StartHaircutCommand startHaircutCommand, [FromServices] IStartHaircutService startHaircutService) =>
+{
+    try
+    {
+        var ret = await startHaircutService.StartHaircut(startHaircutCommand);
+        return Results.Ok(ret);
+    }
+    catch (Exception ex)
+    {
+        return Results.BadRequest(ex);
+    }
+}).WithMetadata(new SwaggerOperationAttribute(summary: "Starts a haircut in the EventStore.", description: "Starts the haircut identified by the HaircutId provided. You can only start a haircut when it is in the waiting status. "))
+  .Produces<string>(StatusCodes.Status200OK)
+  .Produces(StatusCodes.Status400BadRequest);
+
+app.Run();
